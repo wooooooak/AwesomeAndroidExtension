b91f73b08ccba786fc4b86c8f5d41a6e726e8756
이용준 <wooooooak@gmail.com>
2019-06-24 09:59:55 +0900

    Add MutableLiveData default method

---
+++ app/src/main/java/com/wooooooak/awesomeandroidextension/ext/network/livedata/MutableLiveDataExt.kt
@@ -0,0 +1,5 @@
+package com.wooooooak.awesomeandroidextension.ext.network.livedata
+
+import androidx.lifecycle.MutableLiveData
+
+fun <T : Any?> MutableLiveData<T>.default(initialValue: T?) = apply { setValue(initialValue) }
